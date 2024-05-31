3be4253c1d7fa3bbb9ee99ff0206eca5eec0eff9
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-05-31 11:35:08 +0100

    5 again

--- 0x01-python-if_else_loops_functions/5-print_combo2.py
+++ 0x01-python-if_else_loops_functions/5-print_combo2.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i == 9 and j == 9:
-            print("".join("{}{}").format(i, j))
-
-        else:
-            print("".join("{}{}, ").format(i, j), end="")
+print(", ".join("{:02}".format(i) for i in range(100)))
