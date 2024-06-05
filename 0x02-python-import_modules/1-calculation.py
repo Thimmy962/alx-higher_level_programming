92423ca44467d4ade24504cd51fca948c47cd18a
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-06-05 15:01:07 +0100

    0 - 5 again

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-	print("".join("{} + {} = {}".format(a, b, add(a, b))))
-	print("".join("{} - {} = {}".format(a, b, sub(a, b))))
-	print("".join("{} * {} = {}".format(a, b, mul(a, b))))
-	print("".join("{} / {} = {}".format(a, b, div(a, b))))
+    print("".join("{} + {} = {}".format(a, b, add(a, b))))
+    print("".join("{} - {} = {}".format(a, b, sub(a, b))))
+    print("".join("{} * {} = {}".format(a, b, mul(a, b))))
+    print("".join("{} / {} = {}".format(a, b, div(a, b))))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 from sys import argv, exit
 
 len = len(argv)
-if len == 1:
-    print("0 arguments")
-    exit()
+if __name__ == "__main__":
+    if len == 1:
+        print("0 arguments")
+        exit()
 
-elif len == 2:
-    print("".join("{} argument".format(len - 1)))
-else:
-    print("".join("{} arguments".format(len - 1)))
+    elif len == 2:
+        print("".join("{} argument".format(len - 1)))
+    else:
+        print("".join("{} arguments".format(len - 1)))
 
-for i in range(1, len):
-    print(f"{i}: {argv[i]}")
+    for i in range(1, len):
+        print(f"{i}: {argv[i]}")
