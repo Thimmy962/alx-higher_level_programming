bee82e0708af27ee7913a9c8c9af672d1b3deec7
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-06-05 14:54:48 +0100

    did 0 & 1 again

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,8 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print("".join("{} + {} = {}".format(a, b, add(a, b))))
-print("".join("{} - {} = {}".format(a, b, sub(a, b))))
-print("".join("{} * {} = {}".format(a, b, mul(a, b))))
-print("".join("{} / {} = {}".format(a, b, div(a, b))))
+if __name__ == "__main__":
+	print("".join("{} + {} = {}".format(a, b, add(a, b))))
+	print("".join("{} - {} = {}".format(a, b, sub(a, b))))
+	print("".join("{} * {} = {}".format(a, b, mul(a, b))))
+	print("".join("{} / {} = {}".format(a, b, div(a, b))))
