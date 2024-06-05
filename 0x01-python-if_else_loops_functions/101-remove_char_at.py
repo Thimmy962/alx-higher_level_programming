2915d4a62afd4a60cb3118ddf33a737ebb95dab9
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-06-05 12:33:45 +0100

    2nd advanced

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str_1 = ""
+    for (index, char) in enumerate(str):
+        if index == n:
+            continue
+        else:
+            str_1 += char
+    return str_1
