85241adf349c5da5df226493c2492316272eb374
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-06-06 14:20:06 +0100

    added . to 2 line 7

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv, exit
 len = len(argv)
 if __name__ == "__main__":
     if len == 1:
-        print("0 arguments")
+        print("0 argument.")
         exit()
 
     elif len == 2:
