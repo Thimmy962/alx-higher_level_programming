1c7041b057eeed01693abd30230fcedd1aba8895
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-06-18 01:01:49 +0100

    100, 102

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return 0
     numerator = 0
     denominator = 0
-    for  i in my_list:
+    for i in my_list:
         numerator = (i[0] * i[1]) + numerator
         denominator = denominator + i[1]
-    return numerator /denominator
+    return numerator / denominator
